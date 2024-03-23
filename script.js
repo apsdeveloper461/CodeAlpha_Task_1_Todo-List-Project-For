a2e6e850a0083b1eab3bc76fc05286f7ca722ef2
Mehboob Alam <2023se7@student.uet.edu.pk>
2024-03-23 22:00:41 +0500

    Project of Intership is Completed

---
+++ script.js
@@ -0,0 +1,37 @@
+// Handle When click on Add Button and Validate if the input bar is empty
+document.getElementById("search").addEventListener("click", () => {
+  if (document.getElementById("searchBar").value.length == 0) {
+    alert("plz enter some Value and Add it");
+  } else taskCreate(document.getElementById("searchBar").value);
+
+  document.getElementById("searchBar").value = "";
+});
+// Add into todo list after clicking on Add Button
+function taskCreate(value) {
+  document.getElementById("menuTask").innerHTML += `
+    <div class="task">
+    <div class="taskName">${value}</div>
+    <button class="delete">
+     delete
+    </button>
+    </div>`;
+}
+//delete a task on click delete button
+setInterval(() => {
+  var currentTask = document.getElementsByClassName("delete");
+  for (var i = 0; i < currentTask.length; i++) {
+    console.log(i);
+    currentTask[i].onclick = function () {
+      this.parentNode.remove();
+      console.log("remove");
+    };
+  }
+  //Completed Task........
+  var completedTask = document.getElementsByClassName("taskName");
+  for (var i = 0; i < completedTask.length; i++) {
+    completedTask[i].onclick = function () {
+      this.classList.toggle("completed");
+      console.log("Completed");
+    };
+  }
+}, 1000);
